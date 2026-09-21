2a7a9e527c15c53b62ff9dcbd8c4b6533eebbf79
neyugn261 <quocnguyen191112@gmail.com>
2026-09-21 17:37:36 +0700

    test: add Fahrenheit to Celsius test cases

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -17,6 +17,14 @@ class TestConverter(unittest.TestCase):
     def test_fahrenheit_to_celsius_boiling(self):
         self.assertAlmostEqual(fahrenheit_to_celsius(212), 100)
 
+    def test_fahrenheit_to_celsius_negative(self):
+        # -40°F bằng -40°C
+        self.assertAlmostEqual(fahrenheit_to_celsius(-40), -40)
+
+    def test_fahrenheit_to_celsius_decimal(self):
+        # 98.6°F (nhiệt độ cơ thể) tương đương 37°C
+        self.assertAlmostEqual(fahrenheit_to_celsius(98.6), 37, places=2)
+
     def test_negative_values(self):
         # -40 độ C bằng -40 độ F
         self.assertAlmostEqual(celsius_to_fahrenheit(-40), -40)
